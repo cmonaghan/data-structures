7f90c8de721378ef1fc54faeaf4241c43a68eaa4
Christian Monaghan <christian.monaghan@gmail.com>
2013-11-17 15:44:57 -0800

    Passes all tests for stack prototypal pattern

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,15 +1,10 @@
 var makeStack = function() {
   // Hey! Copy your code from src/functional-shared/stack.js and paste it here
-  var instance = {};
+  var instance = Object.create(stackMethods);
 
   // Use an object with numeric keys to store values
   instance.storage = {};
   instance.length = 0; // Hint: set an initial value here
-
-  // Implement the methods below
-  instance.push = stackMethods.push;
-  instance.pop = stackMethods.pop;
-  instance.size = stackMethods.size;
 
   return instance;
 };
