8060275c7cdda0a1606fcb00bda53c905cc09422
Christian Monaghan <christian.monaghan@gmail.com>
2013-11-17 16:13:46 -0800

    Implemented basic enqueue, dequeue, and size methods for functional queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,24 @@ var makeQueue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var length = 0;
 
   // Implement the methods below
 
-  instance.enqueue = function(value){
+  instance.enqueue = function(value){ 
+    storage[length] = value;
+    length++;
   };
 
   instance.dequeue = function(){
+    if (length > 0) {
+      length--;
+      return storage[length];
+    }
   };
 
   instance.size = function(){
+    return length;
   };
 
   return instance;
