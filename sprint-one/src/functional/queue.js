baaa864e37315699b855e3c97107459763051abc
Christian Monaghan <christian.monaghan@gmail.com>
2013-11-17 16:45:00 -0800

    Passes all functional pattern tests for queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -4,6 +4,7 @@ var makeQueue = function(){
   // Use an object with numeric keys to store values
   var storage = {};
   var length = 0;
+  var toRemove = 0; // position of the first item in the queue
 
   // Implement the methods below
 
@@ -13,14 +14,16 @@ var makeQueue = function(){
   };
 
   instance.dequeue = function(){
-    if (length > 0) {
-      length--;
-      return storage[length];
+    if (length - toRemove) { // evaluates to size of queue
+      var firstItem = storage[toRemove];
+      toRemove++;
+      return firstItem;
     }
+
   };
 
   instance.size = function(){
-    return length;
+    return length - toRemove; // size is the difference between the last position and first position
   };
 
   return instance;
